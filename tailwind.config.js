7a17c4df2e832f33e53fdb8c3e4aa80c6fbcfc81
robonexx <robertwagar@gmail.com>
2023-01-10 13:27:15 +0100

    linktree clone

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,css,html}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
